f3419b7eee3374bbf4d99d0122ad7a54251c5566
jvanosdol <j.vanosdol99@yahoo.com>
2023-05-10 16:17:42 -0700

    Add project files.

---
+++ ReverseAString/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ReverseAString.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
